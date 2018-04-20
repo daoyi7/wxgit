61bdf64bd1dcefd03af80dedc3d6979701c46d5f
daoyi7 <codingwell@gmail.com>
2018-04-20 23:21:04 +0800

    init

---
+++ src/components/foot.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="foot">
+    <div class="item">
+      <a href="/pages/counter/main" class="news">News</a>
+    </div>
+    <div class="item">
+      <a href="/pages/counter/main" class="notifications">Notifications</a>
+    </div>
+    <div class="item">
+      <a href="/pages/counter/main" class="profile">Profile</a>
+    </div>
+    <!-- <h2>{{title}}</h2> -->
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped>
+.foot {
+  padding: 0 12px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.item {
+  flex: 1;
+  text-align: center;
+  line-height: 80rpx;
+}
+.item a {
+  font-size: 20rpx;
+}
+</style>
+
---
+++ src/pages/home/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="main">
+      3232
+    </div>
+    <div class="git-foot">
+      <git-foot></git-foot>
+    </div>
+  </div>
+</template>
+
+<script>
+import gitFoot from "@/components/foot";
+export default {
+  components: {
+    gitFoot
+  }
+};
+</script>
+
+<style scoped>
+.main {
+  flex: 1;
+}
+.git-foot {
+  flex: 0 0 80rpx;
+  border-top: .11rpx solid #24292e;
+}
+</style>
